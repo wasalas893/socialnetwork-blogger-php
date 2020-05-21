031979e6b34f78961a2da9021c9a2dc254637cad
wasalas <wasalas893@gmail.com>
2020-05-21 15:56:45 +0530

    creade the main.php

--- main.php
+++ main.php
@@ -3,9 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
+    <title>main</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
+      <div class="row">
+      <div class="col-sm-12">
+         <div class="well">
+           <center><h1>Blogger</h1></center>
+         </div>
+      </div>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-6" style="left: 0.5%;">
+          <img src="images/Travel-Blogger-1024x682 (1).jpg" class="img-rounded" title="Blogger" width="650px"
+          height="565px">
+          <div id="centered1" class="centered">
+              <h3 style="color: red;"><span class="glyphicon glyphicon-search"></span>
+            &nbsp;&nbsp;<strong>Follow Your Interests..</strong></h3>
+          </div>
+        </div>
+      </div>
+
     
 </body>
 </html>
